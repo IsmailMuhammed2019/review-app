8d950fc3de21deb465a40eff1a9834f66012b1c0
Ismail <ismailmuhammed2019@gmail.com>
2021-06-21 17:45:58 +0100

    create dummy data review

---
+++ main.js
@@ -0,0 +1,30 @@
+const reviews = [
+    {
+        id: 1,
+        name: "susan smith",
+        job: "web developer",
+        img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni sunt, molestiae earum suscipit quaerat eligendi. Reiciendis autem aliquid incidunt facere."
+    },
+    {
+        id: 2,
+        name: "anna johson",
+        job: "graphic artist",
+        img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni sunt, molestiae earum suscipit quaerat eligendi. Reiciendis autem aliquid incidunt facere."
+    },
+    {
+        id: 3,
+        name: "peter jones",
+        job: "intern",
+        img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni sunt, molestiae earum suscipit quaerat eligendi. Reiciendis autem aliquid incidunt facere."
+    },
+    {
+        id: 4,
+        name: "bill anderson",
+        job: "the boss",
+        img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni sunt, molestiae earum suscipit quaerat eligendi. Reiciendis autem aliquid incidunt facere."
+    }
+]
